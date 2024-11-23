728d989030c5e753e9c1ab4a3302bfa6885d7707
Sauharda1184 <sauhardameher@gmail.com>
2024-11-23 15:58:20 -0600

    Javadoc Comments

--- Arrayadt.cpp
+++ Arrayadt.cpp
@@ -1,29 +1,36 @@
+/**
+ * @file Arrayadt.cpp
+ * @brief Implementation of a dynamic Array data structure
+ */
+
 #include <iostream>
 using namespace std;
- 
- 
- // A c++ class named Array
-class Array{
- 
- 
- //private access specifier
-private:
 
-    int* A; //dynamic memory allocation of an array in heap
-    int size;//size of an Array
-    int length;//length of an Array
- 
- 
- //public access specifier for member functions
- 
+/**
+ * @class Array
+ * @brief A class that implements dynamic array operations
+ */
+class Array {
+private:
+    int* A;     ///< Pointer to dynamic array in heap
+    int size;   ///< Maximum size of the array
+    int length; ///< Current number of elements in array
+
 public:
- //default constructor
-    Array(int size){
+    /**
+     * @brief Constructor for Array class
+     * @param size The maximum size of the array
+     */
+    Array(int size) {
         this->size = size;
-        A = new int [size];
+        A = new int[size];
     }
- 
-    void create(){
+
+    /**
+     * @brief Creates an array by taking user input
+     * @details Prompts user to enter length and elements of the array
+     */
+    void create() {
         cout << "Enter number of elements: " << flush;
         cin >> length;
         cout << "Enter the array elements: " << endl;
@@ -32,26 +39,33 @@ public:
             cin >> A[i];
         }
     }
- 
-    void display(){
+
+    /**
+     * @brief Displays all elements in the array
+     */
+    void display() {
         for (int i = 0; i < length; i++){
             cout << A[i] << " ";
         }
     }
- 
-    ~Array(){
+
+    /**
+     * @brief Destructor to free allocated memory
+     */
+    ~Array() {
         delete[] A;
         cout << "Array destroyed" << endl;
     }
 };
 
+/**
+ * @brief Main function to demonstrate Array class functionality
+ * @return 0 on successful execution
+ */
 int main() {
- 
- 
     Array arr(10);
     arr.create();
     arr.display();
- 
- 
+
     return 0;
 }
